441a9a1a8b7d035a25c3408031fd7c7405d9f73a
Justin Nguyen <Jnguy129@gmail.com>
2025-09-08 14:52:27 -0500

    Syntax Error Fixed on raindbow mode

--- script.js
+++ script.js
@@ -18,9 +18,18 @@ tools.forEach(button => {
 })
 
 rainbow.addEventListener("click", () => {
-    random != random;
+    random = !random;
     rainbow.classList.toggle('active');
 })
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function createGrid(){
     rangeLabel.textContent = `${range.value} * ${range.value}`;
@@ -73,15 +82,6 @@ function paintWall(wall){
     }else{
         wall.style.backgroundColor = "transparent";
     }
-}
-
-function getRandomColor(){
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for(let i = 0; i < 6; i++){
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
 }
 
 range.addEventListener("input", createGrid);
